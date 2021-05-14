95da78b4edbd815f1a07450f8d0ef52bcfa27905
Jaehwan Ryu <jryu@uncharted.software>
2021-05-14 12:54:29 -0400

    Updated script

--- vector-tiles/update_geojson_properties.py
+++ vector-tiles/update_geojson_properties.py
@@ -14,14 +14,14 @@ with open(file) as geojson_file:
     region_id = properties['NAME_0']
     delimeter = '__'
     if delimeter in region_id:
-      msg = f'Region name, {region_id} contains {delimeter}.\nCan not create an unique region id using region names joined with "_"'
+      msg = f'Region name, {region_id} contains {delimeter}.\nCan not create an unique region id using region names joined with "{delimeter}"'
       raise ValueError(msg)
     for level in range(1, 4): 
       name_field = 'NAME_' + str(level)
       if name_field in properties:
         region_name = properties[name_field] 
         if delimeter in region_name:
-          msg = f'Region name, {region_name} contains {delimeter}.\nCan not create an unique region id using region names joined with "_"' 
+          msg = f'Region name, {region_name} contains {delimeter}.\nCan not create an unique region id using region names joined with "{delimeter}"' 
           raise ValueError(msg)
         region_id = region_id + delimeter + region_name
     properties['id'] = region_id
