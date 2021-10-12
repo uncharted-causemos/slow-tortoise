2674d4cc396069836253af09ee3965961a6d1218
Daniel Bruce <dbruce@uncharted.software>
2021-10-12 09:41:15 -0400

    full solution for download gadm and populate

---
+++ scripts/gadm/scripts/common_gadm.py
@@ -0,0 +1,8 @@
+import os
+import pathlib
+
+MAX_GADM_INDEX = 4
+
+def get_csv_filename(index):
+    this_filename = str(pathlib.Path(__file__).parent.resolve())
+    return this_filename + "/../data/gadm36_{}.csv".format(index)
---
+++ scripts/gadm/scripts/download_gadm_files.py
@@ -0,0 +1,16 @@
+from scripts.gadm.scripts.common_gadm import MAX_GADM_INDEX, get_csv_filename
+import requests
+import os
+
+# Run scripts in this order
+# 1. download_gadm_files.py (this script)
+# 2. populate_with_region_names.py
+
+def download_gadm():
+    for index in range(MAX_GADM_INDEX):
+        csv_url = f"https://data.apps.fao.org/catalog/dataset/code-list-gadm36-global-admin-{index}"
+        req = requests.get(csv_url)
+        url_content = req.content
+        csv_file = open(get_csv_filename(index), 'wb')
+        csv_file.write(url_content)
+        csv_file.close()
