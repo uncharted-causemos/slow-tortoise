8805f3f39ce1567472ffef6092c29ba3273de26b
Ahmed <amostafa@uncharted.software>
2021-11-09 12:11:53 -0500

    refine the generation of bbox by splitting the task per country

--- vector-tiles/generate_bounding_boxes.sh
+++ vector-tiles/generate_bounding_boxes.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
+# Downlaod and process GADM shape files and generate bbox for each region
+
 # Exit the script as soon as one of the commands failed
 set -e
 
-# Downlaod and process GADM shape files and generate bbox for each region
+# Ensure there's a folder to store shape files
+mkdir -p .tmp
+cd ./.tmp
+
+# Clean up previous files
+rm -rf ./gadm*
+rm -rf ./countries
+
+# create a dir to store all country codes/names
+mkdir countries
+
+# download the list of gadm countries and create file for each
+python ../download_and_create_country_files.py
+
+# process each country
+cfiles=($(ls countries/*))
+for f in ${cfiles[@]}; do
+  country=$(echo "$f" | cut -f 1 -d '.' | cut -f 2 -d '/')
+  fname="gadm36_${country}_shp.zip"
+  echo "Downloading shape files for ${country} ..."
+  curl -LO "https://biogeo.ucdavis.edu/data/gadm3.6/shp/${fname}"
+  echo "Extracting ${fname} ..."
+  unzip ${fname} -d "gadm36_${country}_shp" 
+done
 
 # Currently Supported Countries
 # Djibouti: DJI
@@ -15,25 +40,20 @@ set -e
 # Sudan: SDN
 # Ethiopia: ETH
 # Egypt: EGY
-COUNTRIES=("ETH" "SSD" "EGY")
+#COUNTRIES=("ETH")
+#for country in ${COUNTRIES[@]}; do
+#  fname="gadm36_${country}_shp.zip"
+#  echo "Downloading shape files for ${country} ..."
+#  curl -LO "https://biogeo.ucdavis.edu/data/gadm3.6/shp/${fname}"
+#  echo "Extracting ${fname} ..."
+#  unzip ${fname} -d "gadm36_${country}_shp" 
+#done
 
-# Ensure there's a folder to store shape files
-mkdir -p .tmp dist
-cd ./.tmp
-
-# Clean up previous files
-rm -rf ./gadm*
-rm -rf ../dist/*
-
-# Download shape files for all countries
-for country in ${COUNTRIES[@]}; do
-  fname="gadm36_${country}_shp.zip"
-  echo "Downloading shape files for ${country} ..."
-  curl -LO "https://biogeo.ucdavis.edu/data/gadm3.6/shp/${fname}"
-
-  echo "Extracting ${fname} ..."
-  unzip ${fname} -d "gadm36_${country}_shp" 
-done
+#echo "Downloading shape files for all countries ..."
+#curl -LO "https://biogeo.ucdavis.edu/data/gadm3.6/gadm36_shp.zip"
+## https://biogeo.ucdavis.edu/data/gadm3.6/gadm36_levels_shp.zip ## all levels of all countries
+#echo "Extracting gadm36_shp.zip ..."
+#unzip gadm36_shp.zip -d "gadm36_shp" 
 
 # Convert shape files to geojson files
 echo "Converting all shape files to geojson ..."
@@ -46,8 +66,7 @@ for f in ${sfiles[@]}; do
 done
 
 # Extract bbox from geojson shape files for all countries/regions
-python ../extract_bbox_from_shapefiles.py
+python ../extract_bbox_from_geojson_files.py
 
 # Update ES with bbox for all geo regions
-python ../update_gdam_index_with_bbox.py
-
+python ../update_gadm_index_with_bbox.py 
