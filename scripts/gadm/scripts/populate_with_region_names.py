88923c958af75d89c59b1cf2e2359c5d8170fe44
Maciej Kozlowski <mkozlowski@uncharted.software>
2021-11-11 13:50:13 -0500

    PR comments

--- scripts/gadm/scripts/populate_with_region_names.py
+++ scripts/gadm/scripts/populate_with_region_names.py
@@ -15,7 +15,7 @@ def populate_es_with_gadm():
     elastic = Elasticsearch(ES_URL)
 
     files = [get_csv_filename(i) for i in range(MAX_GADM_INDEX)]
-    # column_data must be ordered from broadest region to most specific region (largest to smallest)
+    # column_data and code_columns must be ordered from broadest region to most specific region (largest to smallest)
     column_data = [
         { "input": "name_0", "output": "country" },
         { "input": "name_1", "output": "admin1" },
@@ -23,11 +23,6 @@ def populate_es_with_gadm():
         { "input": "name_3", "output": "admin3" }
     ]
     code_columns = [ "gadm36_0", "gadm36_1", "gadm36_2", "gadm36_3" ]
-
-    def generate_id(filtered_row):
-        sorted_row = sorted([(key, filtered_row[key]) for key in filtered_row])
-        row_as_string = ":".join([f"{key[0]},{key[1]}" for key in sorted_row])
-        return str(uuid.uuid3(uuid.NAMESPACE_DNS, row_as_string))
 
     for file in files:
         with open(file) as f:
