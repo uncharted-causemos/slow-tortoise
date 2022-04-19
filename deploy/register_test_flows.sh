b65273510272b0afab7be6ebe4f08f8679ab867c
dchang <dchang@uncharted.software>
2022-04-19 12:48:25 -0400

    move deployment-related scripts to own folder

---
+++ deploy/register_test_flows.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# agent reads this env var to find the prefect server
+export PREFECT__SERVER__HOST=http://10.65.18.52
+
+# set this to true if images should be pushed to the docker registry as part of the
+# registration process - not necessary if testing locally
+export WM_PUSH_IMAGE=true
+
+PROJECT="project"
+
+prefect register --project="$PROJECT" --label wm-prefect-server.openstack.uncharted.software --label docker --path ../flows/test/flow_test.py
+prefect register --project="$PROJECT" --label wm-prefect-server.openstack.uncharted.software --label docker --path ../flows/test/dask_flow_test.py
+
