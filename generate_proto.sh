995a27f3e1f927b6b28c25151f80b2291b1996be
Jaehwan Ryu <jryu@uncharted.software>
2022-07-26 15:08:02 -0400

    Updated tiles protobuffer

--- generate_proto.sh
+++ generate_proto.sh
@@ -2,4 +2,4 @@
 
 # Installing protoc: 'brew install protobuf'
 # Make sure you have wm-proto repo in your parent directory
-protoc --proto_path=../wm-proto/proto/ --python_out=. tiles.proto
+protoc --proto_path=../wm-proto/proto/ --python_out=./flows/ tiles.proto
