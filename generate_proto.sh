fe82e0479e89113947707241fca3d1a9ef74f75e
Jaehwan Ryu <jryu@uncharted.software>
2022-07-26 14:56:33 -0400

    Updated dependencies

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# Installing protoc: 'brew install protobuf'
 # Make sure you have wm-proto repo in your parent directory
 protoc --proto_path=../wm-proto/proto/ --python_out=. tiles.proto
