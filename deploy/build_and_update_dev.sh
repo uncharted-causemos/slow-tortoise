fa36e486e55802e39e7a20b8451f4476cf4ea337
dchang <dchang@uncharted.software>
2022-07-29 10:26:23 -0400

    update scripts

--- deploy/build_and_update_dev.sh
+++ deploy/build_and_update_dev.sh
@@ -5,9 +5,9 @@ set -e
 # Host dask-swarm-test                                                             
 #     HostName 10.65.18.107                                                        
 #     User centos
-export DOCKER_IMAGE=docker.uncharted.software/worldmodeler/wm-data-pipeline-dev
-export DOCKER_IMAGE_VERSION=latest
 
+
+source ./test.env
 
 SCRIPT_DIR="$(dirname "$0")"
 pushd $SCRIPT_DIR
@@ -22,7 +22,7 @@ pushd ../infra/docker
 popd
 
 echo "Pulling images to swarms..."
-ssh dask-swarm-test 'docker pull docker.uncharted.software/worldmodeler/wm-data-pipeline-dev:latest'
+ssh dask-swarm-test "docker pull $WM_DATA_PIPELINE_IMAGE"
 
 echo "Restarting swarms..."
 ssh dask-swarm-test 'docker stack deploy --with-registry-auth --compose-file docker-dask-docker-compose.yml dask_swarm'
