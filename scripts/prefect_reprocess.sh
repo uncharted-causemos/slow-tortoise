2f089d5b25fbf5e4a9507dee6c7aecd255bd2726
Jaehwan Ryu <jryu@uncharted.software>
2023-07-17 20:43:49 -0400

    Display error message in right order

--- scripts/prefect_reprocess.sh
+++ scripts/prefect_reprocess.sh
@@ -3,7 +3,7 @@
 print_usage() {
   echo "Usage: $0 [-t OUTPUT_TASKS] ENV_FILE_PATH DATA_ID [DATA_ID ...]"
   echo
-  echo "Re-process the ouput data for provided DATA_ID. DATA_ID is either dataset id for indicator dataset or model run id for model run"
+  echo "Re-process the ouput data for provided DATA_ID. DATA_ID is either dataset id for indicator dataset or model run id for model run."
   echo 
   echo "Options:"
   echo "  -t OUTPUT_TASKS     Set selected output tasks to be ran. OUTPUT_TASKS is a comma seperated list of output tasks. e.g. 'compute_regional_stats,compute_regional_timeseries' "
@@ -12,23 +12,17 @@ print_usage() {
   echo
   echo "Examples:"
   echo "  $0 ./env/analyst.env 7ae6001f-2227-4e1b-8ac1-10bd012325e2 b9232bf2-b8a3-4718-bb94-08f0254415d9" # 
-  echo "    Run with multiple dataset ids" # 
+  echo "    Run with multiple dataset ids"
   echo
   echo "  $0 ./env/analyst.env \$(< datasetids.txt)"
-  echo "    Run with a file, datasetids.txt containing list of dataset ids" # 
+  echo "    Run with a file, datasetids.txt containing list of dataset ids"
   echo
   echo "  $0 -t compute_regional_stats,compute_regional_timeseries ./env/analyst.env 7ae6001f-2227-4e1b-8ac1-10bd012325e2" # 
-  echo "    Run with selected output tasks" # 
+  echo "    Run with selected output tasks"
   echo 
   echo "  $0 -m -t compute_regional_stats,compute_regional_timeseries ./env/analyst.env 01ad71a7-7c66-4738-92d9-28581770c73a" # 
-  echo "    Reprocess a model run" # 
+  echo "    Reprocess a model run with model run id"
 }
-
-if [ $# -lt 2 ]; then
-  echo "Error: Required arguments missing."
-  print_usage
-  exit 1
-fi
 
 data_output_type="indicators"
 run_script="prefect_reprocess_indicator.py"
@@ -57,6 +51,12 @@ while getopts ":t:mh" opt; do
 done
 # Shift the option arguments to skip past them. Now $1 refers to the first arugmet, ENV_FILE_PATH
 shift "$((OPTIND - 1))"
+
+if [ $# -lt 2 ]; then
+  echo "Error: Required arguments missing."
+  print_usage
+  exit 1
+fi
 
 source $1 
 echo "Running Configuration"
